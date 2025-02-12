5d257a31f19ca86e3ce61216d32663f3f28d7d52
Jango-7 <jhoanngo2@gmail.com>
2025-02-12 11:25:32 -0300

    Calculo de area de un circulo

---
+++ area-circulo.py
@@ -0,0 +1,8 @@
+from math import pi
+
+
+radio = float(input("Por favor, ingrese el radio del circulo: "))
+
+area = pi * radio ** 2
+
+print(f'El area del circulo con radio {radio} es: {round(area, 2)}')
